b66a2ee42651e893fd8d70230aef1471b82f584e
Shyamsundar <shyamsundar59807@gmail.com>
2026-02-07 20:39:41 +0530

    config updated

--- taskbuddy_backend/src/main/java/com/taskbuddy/security/SecurityConfiguration.java
+++ taskbuddy_backend/src/main/java/com/taskbuddy/security/SecurityConfiguration.java
@@ -83,7 +83,7 @@ public class SecurityConfiguration {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 	    CorsConfiguration configuration = new CorsConfiguration();
-	    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+	    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "https://front-end-ebon-eight.vercel.app"));
 	    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 	    configuration.setAllowedHeaders(Arrays.asList("*"));
 	    configuration.setAllowCredentials(true);
